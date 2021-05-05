90e723fd4fb1e9d2d31a6fb1ae12970c58aee029
idrisuiforchange <idrisuidev@gmail.com>
2021-05-05 21:26:15 +0530

    duplicateTransactionMethod

--- src/find-duplicates.spec.ts
+++ src/find-duplicates.spec.ts
@@ -1,57 +1,99 @@
-import { findDuplicateTransactions, Transaction } from './find-duplicates';
+import { findDuplicateTransactions, Transaction } from "./find-duplicates";
 
-test('Find duplicate transactions', () => {
-  const transactions: Transaction[] = [
+test("Duplicate Transaction negative Scenrio", () => {
+  const transactionsNotDup: Transaction[] = [
     {
       id: 1,
-      source: 'A',
-      target: 'B',
+      source: "A",
+      target: "B",
       amount: 300,
-      description: 'tikkie'
+      description: "tikkie"
     },
     {
       id: 2,
-      source: 'B',
-      target: 'C',
+      source: "B",
+      target: "C",
       amount: 1000,
-      description: 'rent'
+      description: "rent"
     },
     {
       id: 3,
-      source: 'A',
-      target: 'B',
-      amount: 300,
-      description: 'tikkie'
+      source: "E",
+      target: "Z",
+      amount: 340,
+      description: "Home"
     },
     {
       id: 4,
-      source: 'A',
-      target: 'D',
+      source: "A",
+      target: "D",
       amount: 100,
-      description: 'groceries'
+      description: "groceries"
     },
     {
       id: 5,
-      source: 'A',
-      target: 'C',
+      source: "A",
+      target: "C",
       amount: 250,
-      description: 'other'
+      description: "other"
+    }
+  ];
+
+  expect(findDuplicateTransactions(transactionsNotDup)).toEqual([]);
+});
+
+test("Find duplicate transactions", () => {
+  const transactions: Transaction[] = [
+    {
+      id: 1,
+      source: "A",
+      target: "B",
+      amount: 300,
+      description: "tikkie"
+    },
+    {
+      id: 2,
+      source: "B",
+      target: "C",
+      amount: 1000,
+      description: "rent"
+    },
+    {
+      id: 3,
+      source: "A",
+      target: "B",
+      amount: 300,
+      description: "tikkie"
+    },
+    {
+      id: 4,
+      source: "A",
+      target: "D",
+      amount: 100,
+      description: "groceries"
+    },
+    {
+      id: 5,
+      source: "A",
+      target: "C",
+      amount: 250,
+      description: "other"
     }
   ];
   expect(findDuplicateTransactions(transactions)).toEqual([
     {
       id: 1,
-      source: 'A',
-      target: 'B',
+      source: "A",
+      target: "B",
       amount: 300,
-      description: 'tikkie'
+      description: "tikkie"
     },
     {
       id: 3,
-      source: 'A',
-      target: 'B',
+      source: "A",
+      target: "B",
       amount: 300,
-      description: 'tikkie'
+      description: "tikkie"
     }
   ]);
 });
--- src/find-duplicates.ts
+++ src/find-duplicates.ts
@@ -1,14 +1,18 @@
 export interface Transaction {
-  // TODO
+  id: number;
+  source: string;
+  target: string;
+  amount: number;
+  description: string;
 }
 
 /**
  * Find duplicate transactions
  * Given a list of transactions, find and return duplicate transactions. There can be more than one duplicate transactions.
  * A transaction can be called duplicate if it has the same `source`, `target`, `amount` and `description`.
- * 
+ *
  * This is how a transaction looks like.
- * 
+ *
  * {
  *   id: 1,
  *   source: 'A',
@@ -16,34 +20,39 @@ export interface Transaction {
  *   amount: 300,
  *   description: 'tikkie'
  * }
- * 
+ *
  * Note:
  * - Create additional functions if required.
  * - Follow proper coding conventions and best practices.
  * - Make sure existing unit test passes.
  * - Write further unit tests to cover maximum code.
- *  
- * 
+ *
+ *
  * @param transactions List of transactions
  * @returns {Transaction[]} List of duplicate transactions
  */
-export function findDuplicateTransactions(transactions: Transaction[]): Transaction[] {
-  // TODO
-  // This has been done just to make the test pass for now.
-  return [
-    {
-      id: 1,
-      source: 'A',
-      target: 'B',
-      amount: 300,
-      description: 'tikkie'
+export function findDuplicateTransactions(
+  transactions: Transaction[]
+): Transaction[] {
+  let duplicates: Transaction[] = [];
+  const filteredArr1 = transactions.reduce(
+    (findDuparr: Transaction[], current: Transaction) => {
+      let x = findDuparr.filter(
+        val =>
+          val.source === current.source &&
+          val.amount === current.amount &&
+          val.description === current.description &&
+          val.target === current.target
+      );
+      if (x.length === 0) {
+        return findDuparr.concat([current]);
+      } else {
+        duplicates.push(...x);
+        duplicates.push(current);
+        return findDuparr;
+      }
     },
-    {
-      id: 3,
-      source: 'A',
-      target: 'B',
-      amount: 300,
-      description: 'tikkie'
-    }
-  ];
+    []
+  );
+  return duplicates;
 }
